5a2aee2c83100018114f7e9db98a3bec6aa7cc3c
charles blundell <cb+git@glia.me.uk>
2010-11-01 00:48:31 +0000

    remove debug log()s

--- static/mutorus.js
+++ static/mutorus.js
@@ -1,5 +1,6 @@
 var root='';
 var want_new = false;
+var queue = [];
 
 function log(msg) {
     setTimeout(function() {
@@ -8,39 +9,37 @@ function log(msg) {
 }
 
 $(document).ready(function () {
-    //$("#qs").click(function () {$("#qf").submit(); return false;});
     $("#qn").click(function () { log('button next');want_new = true; next(); return false;});
     $("#qf").submit(function () { want_new = true; lk(); return false;});
 });
+
 function onYouTubePlayerReady(playerId) {
     var ytplay = document.getElementById("ytplay");
     ytplay.addEventListener("onStateChange", "sc");
     ytplay.addEventListener("onError", "next");
 }
 
-function sc(state) { log('state is ' + state); if (state == 0) { next(); } }
-var queue = [];
+function sc(state) { if (state == 0) { next(); } }
+
 function next() {
-    log('next?');
     if (queue.length > 0) {
         var ytplay = document.getElementById("ytplay");
         var vid = queue.pop();
-        log('now playing ' + vid.url);
         ytplay.loadVideoByUrl(vid.url,0);
         want_new = false;
     }
     if (queue.length < 3) {
-        log('want more');
         lk();
     }
-} 
+}
+
 function lk() {
     var qq = $('input[name="q"]').val();
     $.getJSON(root+'/z', {q: qq},up);
     return false;
 }
+
 function up(data) {
-    log('data: ' +data.vs.length);
     for (var ii = 0; ii < data.vs.length; ii++) {
         queue.push(data.vs[ii]);
     }
