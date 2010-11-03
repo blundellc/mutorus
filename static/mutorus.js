64f7b7c659befee11e2c9882001d27c4e33edb37
charles blundell <cb+git@glia.me.uk>
2010-11-03 09:45:57 +0000

    hashtrick; display title+link; be a bit more aggressive about playing next video

--- static/mutorus.js
+++ static/mutorus.js
@@ -2,6 +2,7 @@ var root='';
 var want_new = false;
 var queue = [];
 var offset = 1;
+var hash = 'NBDAunPkSAY';
 
 function log(msg) {
     setTimeout(function() {
@@ -12,7 +13,12 @@ function log(msg) {
 $(document).ready(function () {
     $("#qn").click(function () { want(); next(); return false;});
     $("#qf").submit(function () { want(); queue=[]; offset=1; lk(); return false;});
+    var params = { allowScriptAccess: "always" };
+    var atts = { id: "ytplay" };
+    restore();
+    swfobject.embedSWF("http://www.youtube.com/v/"+hash+"?enablejsapi=1&playerapiid=ytplayer", "ytframe", "425", "356", "8", null, null, params, atts);
 });
+
 
 function onYouTubePlayerReady(playerId) {
     var ytplay = document.getElementById("ytplay");
@@ -30,19 +36,48 @@ function err() {
     next();
 }
 
-function got() {
-    $('#q').css('background-color','');
+function got(vid) {
     want_new = false;
+    var ytplay = document.getElementById("ytplay");
+    ytplay.loadVideoByUrl(vid.url,0);
+    ytplay.playVideo();
+    document.title = vid.title+' (mutorus)';
+    $('#q').css('background-color','');
+    $('#msg').text(vid.title);
+    $('#msg').attr('href',vid.url);
+    hash = vid.url.replace(/^http:..www.youtube.com(.watch)?.v.([^&?]+)([?&].*)?$/, "$2");
+    save();
 }
 
-function sc(state) { if (state == 0) { next(); } }
+function save() {
+    var q = $('#q').val();
+    if (q) {
+        location.hash = hash+'/'+q;
+    } else {
+        location.hash = hash+'/';
+    }
+}
+
+function restore() {
+    log('restore ' + location.hash);
+    if (location.hash && location.hash != '/') {
+        var h = location.hash.slice(1).split('/',2);
+        hash = h.shift();
+        var q = h.shift();
+        for (var i = 0; i < h.length; i++) {
+            q = q + '/' + h[i];
+        }
+        $('#q').val(q);
+        lk();
+    }
+}
+
+function sc(state) { log('state ' + state); if (state == 0) { next(); } }
 
 function next() {
+    log('next');
     if (queue.length > 0) {
-        var ytplay = document.getElementById("ytplay");
-        var vid = queue.pop();
-        ytplay.loadVideoByUrl(vid.url,0);
-        got();
+        got(queue.pop());
     }
     if (queue.length < 3) {
         lk();
@@ -64,9 +99,4 @@ function up(data) {
     }
 }
 
-(function(){
-var params = { allowScriptAccess: "always" };
-var atts = { id: "ytplay" };
-swfobject.embedSWF("http://www.youtube.com/v/NBDAunPkSAY?enablejsapi=1&playerapiid=ytplayer", "ytframe", "425", "356", "8", null, null, params, atts);
-})();
 
