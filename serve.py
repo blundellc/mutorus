d055438e3e153a631fff26db5d9a4d13d71ae2e2
charles blundell <cb+git@glia.me.uk>
2010-11-04 09:11:45 +0000

    cache results. deal with hashes not swf urls (seems more robust).

--- serve.py
+++ serve.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, g, request, jsonify
+from werkzeug.contrib.cache import SimpleCache
 import gdata.youtube
 import gdata.youtube.service
-import random
+import random, re
 
 app = Flask(__name__)
+cache = SimpleCache()
 
 @app.route('/')
 def index():
@@ -18,26 +20,42 @@ def suggest():
     yt = gdata.youtube.service.YouTubeService()
     yt.ssl = False
     keywords = [keyword.strip() for keyword in q.split(',')]
-    qs = [mkquery(keyword, o) for keyword in keywords]
-    results = [{'title':v.media.title.text,
-                'duration':v.media.duration.seconds,
-                'url':v.GetSwfUrl()}
-               for qq in qs
-               for v in yt.YouTubeQuery(qq).entry]
-    if len(qs):
-        last = len(results)//len(qs)
+    results = [res
+               for keyword in keywords
+               for res in ytquery(yt, keyword, o)]
+    if len(keywords):
+        last = len(results)//len(keywords)
     else:
         last = 0
     random.shuffle(results)
     return jsonify(vs=results, last=last)
 
-def mkquery(keywords, offset):
-    qq = gdata.youtube.service.YouTubeVideoQuery()
-    qq.vq = keywords
-    qq.orderby = 'relevance'
-    qq.racy = 'exclude'
-    qq.start_index = offset
-    return qq
+watch_url_hash = re.compile(r'^http:..www.youtube.com(.watch)?.v.([^&?]+)([?&].*)?$')
+vid_timeout = 6 * 60 * 60
+
+def ytquery(yt, keywords, offset):
+    res = cache.get((keywords, offset))
+    if res is None:
+        qq = gdata.youtube.service.YouTubeVideoQuery()
+        qq.vq = keywords
+        qq.orderby = 'relevance'
+        qq.racy = 'exclude'
+        qq.start_index = offset
+        qq.max_results = 5
+        res = []
+        for v in yt.YouTubeQuery(qq).entry:
+            url = v.GetSwfUrl()
+            if url is None:
+                url = v.media.player.url
+                if url is None:
+                    continue
+            hh = watch_url_hash.sub(r'\2', url)
+            res.append({'title':v.media.title.text,
+                        'duration':v.media.duration.seconds,
+                        'watch':v.media.player.url,
+                        'hash':hh})
+        cache.set((keywords,offset), res, timeout=vid_timeout)
+    return res
 
 if __name__ == '__main__':
     app.debug = True
--- static/mutorus.js
+++ static/mutorus.js
@@ -1,4 +1,5 @@
-var root='';
+//(function() {
+
 var want_new = false;
 var queue = [];
 var offset = 1;
@@ -11,14 +12,43 @@ function log(msg) {
 }
 
 $(document).ready(function () {
-    $("#qn").click(function () { want(); next(); return false;});
+    $("#qn").click(function () { next(); return false;});
     $("#qf").submit(function () { want(); queue=[]; offset=1; lk(); return false;});
-    var params = { allowScriptAccess: "always" };
-    var atts = { id: "ytplay" };
     restore();
-    swfobject.embedSWF("http://www.youtube.com/v/"+hash+"?enablejsapi=1&playerapiid=ytplayer", "ytframe", "425", "356", "8", null, null, params, atts);
 });
 
+function want() {
+    $('#q').css('background-color','yellow');
+    want_new = true;
+}
+
+function watch(vid) {
+    play(vid.hash);
+    want_new = false;
+    hash = vid.hash;
+    document.title = vid.title+' (mutorus)';
+    $('#q').css('background-color','');
+    $('#msg').text(vid.title);
+    $('#msg').attr('href',vid.watch);
+    save();
+}
+
+
+function play(hash) {
+    var ytplay = document.getElementById("ytplay");
+    if (!ytplay) {
+        loadswf(hash);
+    } else {
+        ytplay.loadVideoById(hash,0);
+        ytplay.playVideo();
+    }
+}
+
+function loadswf(hh) {
+    var params = { allowScriptAccess: "always" };
+    var atts = { id: "ytplay" };
+    swfobject.embedSWF("http://www.youtube.com/v/"+hh+"?enablejsapi=1&playerapiid=ytplayer&autoplay=1", "ytframe", "425", "356", "8", null, null, params, atts);
+}
 
 function onYouTubePlayerReady(playerId) {
     var ytplay = document.getElementById("ytplay");
@@ -26,68 +56,41 @@ function onYouTubePlayerReady(playerId) {
     ytplay.addEventListener("onError", "err");
 }
 
-function want() {
-    $('#q').css('background-color','yellow');
-    want_new = true;
-}
 
-function err() {
-    log('yt error');
-    next();
-}
-
-function got(vid) {
-    want_new = false;
-    var ytplay = document.getElementById("ytplay");
-    ytplay.loadVideoByUrl(vid.url,0);
-    ytplay.playVideo();
-    document.title = vid.title+' (mutorus)';
-    $('#q').css('background-color','');
-    $('#msg').text(vid.title);
-    $('#msg').attr('href',vid.url);
-    hash = vid.url.replace(/^http:..www.youtube.com(.watch)?.v.([^&?]+)([?&].*)?$/, "$2");
-    save();
-}
+function err() { log('yt error ' + JSON.stringify(arguments)); next(); }
+function sc(state) { if (state == 0) { next(); } }
 
 function save() {
     var q = $('#q').val();
-    if (q) {
-        location.hash = hash+'/'+q;
-    } else {
-        location.hash = hash+'/';
-    }
+    location.hash = hash+'/'+offset+'/'+(q?q:'');
 }
 
 function restore() {
-    log('restore ' + location.hash);
     if (location.hash && location.hash != '/') {
-        var h = location.hash.slice(1).split('/',2);
+        var h = location.hash.slice(1).split('/');
         hash = h.shift();
+        offset = parseInt(h.shift());
         var q = h.shift();
         for (var i = 0; i < h.length; i++) {
             q = q + '/' + h[i];
         }
         $('#q').val(q);
         lk();
+        watch({'hash':hash,'watch':'','title':q});
     }
 }
 
-function sc(state) { log('state ' + state); if (state == 0) { next(); } }
-
 function next() {
-    log('next');
+    want();
     if (queue.length > 0) {
-        got(queue.pop());
+        watch(queue.pop());
     }
     if (queue.length < 3) {
         lk();
     }
 }
 
-function lk() {
-    var qq = $('#q').val();
-    $.getJSON(root+'/z', {q: qq, o: offset},up);
-}
+function lk() { $.getJSON('/z', {q: $('#q').val(), o: offset},up); }
 
 function up(data) {
     for (var ii = 0; ii < data.vs.length; ii++) {
@@ -99,4 +102,4 @@ function up(data) {
     }
 }
 
-
+//})();
